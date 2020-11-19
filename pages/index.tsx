14c79db27e79b3222fed0d86e6285e7efb7f1301
wangxincheng <xincheng.wang@hand-china.com>
2020-11-19 14:46:13 +0800

    fix: 修改css规范

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 import React from 'react';
 
-import styles from '../styles/Home.module.css';
+const styles = require('../styles/Home.module.css');
 
 export default function Home() {
   return (
