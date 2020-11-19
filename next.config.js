cbf425c09001ad2eec662a326bb333a706b552a1
wangxincheng <xincheng.wang@hand-china.com>
2020-11-19 15:56:41 +0800

    add: 添加antd组件库

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
-  /* config options here */
-};
+const withCss = require('@zeit/next-css');
+
+if (typeof require !== 'undefined') {
+  require.extensions['.css'] = () => {};
+}
+
+module.exports = withCss({
+  compress: false,
+  devIndicators: {
+    autoPrerender: false,
+  },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
+  reactStrictMode: true,
+});
