d484acd014da98d0b9b5b5623d2ae8f1e521b1ff
wangxincheng <xincheng.wang@hand-china.com>
2020-11-25 16:35:45 +0800

    fix: 修改next配置

--- next.config.js
+++ next.config.js
@@ -17,3 +17,16 @@
 //   },
 //   reactStrictMode: true,
 // });
+module.exports ={
+  compress: false,
+  devIndicators: {
+    autoPrerender: false,
+  },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 25 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 2,
+  },
+  reactStrictMode: true,
+};
