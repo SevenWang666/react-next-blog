8582ff76faa53b72377a6c99ccdd1f0ca95c4f0f
wangxincheng <xincheng.wang@hand-china.com>
2020-11-25 16:31:24 +0800

    delete: 移除antd相关组件

--- pages/settings/users.tsx
+++ pages/settings/users.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Progress } from 'antd';
 import Link from 'next/link';
 
 class Users extends React.Component {
   render () {
     return (
       <div style={{ width: 170 }}>
-        <Progress percent={30} size="small" />
-        <Progress percent={50} size="small" status="active" />
-        <Progress percent={70} size="small" status="exception" />
-        <Progress percent={100} size="small" />
         <Link href="/">come back </Link>
       </div>
     );
