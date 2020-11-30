9a993ef60cd0932bcac3f4ce9ced9c958e122709
wangxincheng <xincheng.wang@hand-china.com>
2020-11-30 21:22:08 +0800

    fix: 修改pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,6 +48,9 @@ export default function Home ({
         <Button variant="contained" color="primary">
           你好，世界,我将开始创建博客网站
         </Button>
+        <Link href='/settings/users'>
+          <a>登录注册</a>
+        </Link>
       </section>
     </Layout>
   );
