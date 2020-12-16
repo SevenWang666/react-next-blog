1bac0b83a51521cdcfd2d35a7bcb26c55c163ee1
wangxincheng <xincheng.wang@hand-china.com>
2020-12-16 16:43:43 +0800

    fix: login登录

--- pages/settings/login.tsx
+++ pages/settings/login.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import next from 'next/link';
 import { FormControl,InputLabel,FormHelperText,Input} from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }),
+);
 
+const classes = useStyles();
 class Login extends React.Component {
   constructor (props){
     super(props);
@@ -10,16 +23,25 @@ class Login extends React.Component {
   render (){
     return(
       <>
-        <FormControl>
-          <InputLabel htmlFor="my-input">Email address</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" />
-          <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor="my-input">Email address</InputLabel>
-          <Input id="my-input" aria-describedby="my-helper-text" />
-          <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-        </FormControl>
+        <div>
+          <FormControl>
+            <InputLabel htmlFor="my-input">账号</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">账号</FormHelperText>
+          </FormControl>
+        </div>
+        <div>
+          <FormControl>
+            <InputLabel htmlFor="my-input">密码</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">密码</FormHelperText>
+          </FormControl>
+        </div>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Standard" />
+          <TextField id="filled-basic" label="Filled" variant="filled" />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
       </>
     );
   }
