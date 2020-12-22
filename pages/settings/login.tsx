69a47db0cf59782175488f2a6d5bbebab82dac0e
wangxincheng <xincheng.wang@hand-china.com>
2020-12-22 11:42:49 +0800

    fix: css in js

--- pages/settings/login.tsx
+++ pages/settings/login.tsx
@@ -1,43 +1,73 @@
 import React from 'react';
-import next from 'next/link';
 import { FormControl,InputLabel,FormHelperText,Input} from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme,withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     root: {
-//       '& > *': {
-//         margin: theme.spacing(1),
-//         width: '25ch',
-//       },
-//     },
-//   }),
-// );
-
-// const classes = useStyles();
-class Login extends React.Component {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }),
+);
+const styles={
+  root:{
+    color:'red',
+    padding:'0 30px'
+  }
+};
+// function Login (){
+//   const classes = useStyles();
+//   return(
+//     <>
+//       <div>
+//         <FormControl>
+//           <InputLabel htmlFor="my-input">账号</InputLabel>
+//           <Input id="my-input" aria-describedby="my-helper-text" />
+//           <FormHelperText id="my-helper-text">账号</FormHelperText>
+//         </FormControl>
+//       </div>
+//       <div>
+//         <FormControl>
+//           <InputLabel htmlFor="my-input">密码</InputLabel>
+//           <Input id="my-input" aria-describedby="my-helper-text" />
+//           <FormHelperText id="my-helper-text">密码</FormHelperText>
+//         </FormControl>
+//       </div>
+//       <form className={classes.root} noValidate autoComplete="off">
+//         <TextField id="standard-basic" label="Standard" />
+//         <TextField id="filled-basic" label="Filled" variant="filled" />
+//         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+//       </form>
+//     </>
+//   );
+// }
+class Login extends React.Component<{classes}> {
   constructor (props){
     super(props);
   }
   render (){
+    const {classes} = this.props;
     return(
       <>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">账号</InputLabel>
             <Input id="my-input" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">账号</FormHelperText>
+            <FormHelperText id="my-helper-text">账号123</FormHelperText>
           </FormControl>
         </div>
         <div>
           <FormControl>
             <InputLabel htmlFor="my-input">密码</InputLabel>
             <Input id="my-input" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">密码</FormHelperText>
+            <FormHelperText id="my-helper-text">密码123</FormHelperText>
           </FormControl>
         </div>
-        <form  noValidate autoComplete="off">
+        <form className={classes.root} noValidate autoComplete="off">
           <TextField id="standard-basic" label="Standard" />
           <TextField id="filled-basic" label="Filled" variant="filled" />
           <TextField id="outlined-basic" label="Outlined" variant="outlined" />
@@ -46,4 +76,4 @@ class Login extends React.Component {
     );
   }
 }
-export default Login;
+export default withStyles(styles)(Login);
