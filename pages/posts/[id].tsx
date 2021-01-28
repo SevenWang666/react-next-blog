8a5966c4a93f4140331648ab4615abe267ea8670
wangxincheng <xincheng.wang@hand-china.com>
2021-01-28 18:08:33 +0800

    fix: 加入标题

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -21,6 +21,8 @@ export default function Post ({
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
