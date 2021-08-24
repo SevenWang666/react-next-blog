72399bed680a81f79dd206c46f284419d4845b5a
wangxincheng <xincheng.wang@hand-china.com>
2021-08-24 18:34:16 +0800

    fix: 修改日志文件

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.prepare().then(() => {
     }
   }).listen(3001, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log('> Ready on http://localhost:3001');
   });
 });
