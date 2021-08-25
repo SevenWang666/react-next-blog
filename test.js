62efc181fed0860f97910c91f2fd9c34d07e2e2d
wangxincheng <xincheng.wang@hand-china.com>
2021-08-25 08:56:03 +0800

    fix: 添加

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-let regexp = /a*?/g;
-let str = 'aaabb';
-let array = [...str.match(regexp)];
-console.log(array);
+// let regexp = /a*?/g;
+// let str = 'aaabb';
+// let array = [...str.match(regexp)];
+
+
+
+var template = 'the little cat cat cat cat cat cat2 is in the hat hat hat2, we like it.';
+var regex =/(\w+)(\s(?<!\w)\1(?!\w))+/g;
+console.log(template.replace(regex,'$1'));
